4de584d69bc06ccada4116e53c67c14829201bb7
Dashamir Hoxha <dashohoxha@gmail.com>
2017-08-28 11:12:18 +0200

    Use the recommended version of docker options.

--- cmd/create.sh
+++ cmd/create.sh
@@ -9,5 +9,6 @@ _EOF
 rename_function cmd_create orig_cmd_create
 cmd_create() {
     mkdir -p home/
-    orig_cmd_create -v $(pwd)/home:/home
+    orig_cmd_create \
+        --mount type=bind,source=$(pwd)/home,destination=/home
 }
